3b81d79761a878b8618c1e1788ec363be9c30856
CecileVexe <cecile.valte@gmail.com>
2024-02-20 22:27:01 +0100

    Feat: CRUD and Seeder table category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,37 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\Category::factory()->create([
+            "name" => "Blonde",
+            "description" => "Nos plus belles bières blondes",
+            "slug" => "blonde",
+            "color" => "orange",
+        ]);
+
+        \App\Models\Category::factory()->create([
+            "name" => "Brune",
+            "description" => "Nos meilleures bières brunes",
+            "slug" => "brune",
+            "color" => "purple",
+        ]);
+
+
+        \App\Models\Category::factory()->create([
+            "name" => "Blanche",
+            "description" => "Nos plus pure bière blanche",
+            "slug" => "blanche",
+            "color" => "yellow",
+        ]);
+
+        $beers = \App\Models\Beer::factory(50)->create();
+
+        $categories = \App\Models\Category::all();
+
+        $beers->each(function ($book) use ($categories) {
+            $book->categories()->attach(
+                $categories->random(rand(1, 2))->pluck("id")->toArray()
+            );
+        });
     }
 }
