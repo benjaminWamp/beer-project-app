7b48fdb99ff0a5f95863ffa773a3685752591d98
Benjamin <benjamin.langlois@devinfluence.fr>
2024-03-22 15:25:42 +0100

    Wip : interface dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Order;
+use App\Models\Product;
+use App\Models\Review;
+use App\Models\Favorite;
 use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
@@ -20,6 +23,16 @@ class DashboardController extends Controller
         ->where(DB::raw("DATE_FORMAT(created_at, '%Y-%m')"), $currentMonth)
         ->count();
 
-        dd($countOrderDelivered);
+        $lastFavoris = DB::table('favorites')
+        ->select(DB::raw('COUNT(*) AS data'), 'product_id')
+        ->groupBy('product_id')
+        ->orderByDesc('data')
+        ->get();
+
+        $listProductEmpty = Product::where("stock", "<", 10)->get();
+
+        $lastReview = Review::where(DB::raw("DATE_FORMAT(created_at, '%Y-%m')"), $currentMonth)->take(3)->get();
+
+        return view('Dashboard.dashboard', ["totalSold" => $totalSold, "countOrderInProgress" => $countOrderInProgress, "countOrderDelivered" => $countOrderDelivered, "listProductEmpty" => $listProductEmpty, "lastReview" => $lastReview]);
     }
 }
--- resources/views/Dashboard/dashboard.blade.php
+++ resources/views/Dashboard/dashboard.blade.php
@@ -1,3 +1,61 @@
 <x-layout>
-    
+    <div class="grid grid-cols-3 mt-4 gap-4">
+        <div class="rounded-md border-solid border-2 p-4 flex justify-between">
+            <div>
+                <p class="font-bold">{{$totalSold}}€</p>
+                <p>Ce mois-ci</p>
+            </div>
+            <div class="flex items-center w-12">
+                <svg class="bg-accent rounded-full p-1 w-12 h-12 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.6 16.733c.234.269.548.456.895.534a1.4 1.4 0 0 0 1.75-.762c.172-.615-.446-1.287-1.242-1.481-.796-.194-1.41-.861-1.241-1.481a1.4 1.4 0 0 1 1.75-.762c.343.077.654.26.888.524m-1.358 4.017v.617m0-5.939v.725M4 15v4m3-6v6M6 8.5 10.5 5 14 7.5 18 4m0 0h-3.5M18 4v3m2 8a5 5 0 1 1-10 0 5 5 0 0 1 10 0Z"/>
+                </svg>
+            </div>
+        </div>
+        <div class="rounded-md border-solid border-2 p-4 flex justify-between">
+            <div>
+                <p class="font-bold">{{$countOrderInProgress}}</p>
+                <p>Livraison en cours</p>
+            </div>
+            <div class="flex items-center w-12">
+                <svg class="bg-accent rounded-full p-1 w-12 h-12 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7h-1M8 7h-.688M13 5v4m-2-2h4"/>
+                </svg>
+
+            </div>
+        </div>
+        <div class="rounded-md border-solid border-2 p-4 flex justify-between">
+            <div>
+                <p class="font-bold">{{$countOrderDelivered}}</p>
+                <p>Livré ce mois</p>
+            </div>
+            <div class="flex items-center w-12">
+                <svg class="bg-accent rounded-full p-1 w-12 h-12 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 8h6m-6 4h6m-6 4h6M6 3v18l2-2 2 2 2-2 2 2 2-2 2 2V3l-2 2-2-2-2 2-2-2-2 2-2-2Z"/>
+                </svg>
+
+            </div>
+        </div>
+    </div>
+    <div>
+        <p>Trois derniers avis</p>
+        @foreach($lastReview as $data)
+        <div>
+            <p>{{$data->user->name}} - {{$data->product->name}}</p>
+            <div class="flex ">
+                @for($i = 1; $i < 6; $i++)
+                    @if($i <= $data->stars)
+                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                        <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"/>
+                    </svg>
+                    @else 
+                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-width="2" d="M11.083 5.104c.35-.8 1.485-.8 1.834 0l1.752 4.022a1 1 0 0 0 .84.597l4.463.342c.9.069 1.255 1.2.556 1.771l-3.33 2.723a1 1 0 0 0-.337 1.016l1.03 4.119c.214.858-.71 1.552-1.474 1.106l-3.913-2.281a1 1 0 0 0-1.008 0L7.583 20.8c-.764.446-1.688-.248-1.474-1.106l1.03-4.119A1 1 0 0 0 6.8 14.56l-3.33-2.723c-.698-.571-.342-1.702.557-1.771l4.462-.342a1 1 0 0 0 .84-.597l1.753-4.022Z"/>
+                    </svg>
+                    @endif
+                @endfor
+            </div>
+            <p class="truncate max-w-auto">{{$data->message}}</p>
+        </div>
+        @endforeach
+    </div>    
 </x-layout>
