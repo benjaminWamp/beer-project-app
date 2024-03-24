ad2d3f72216421e8645d8023dd27651b1fad1cce
Benjamin <benjamin.langlois@devinfluence.fr>
2024-03-24 19:17:00 +0100

    Wip : table produit

--- resources/views/Dashboard/dashboard.blade.php
+++ resources/views/Dashboard/dashboard.blade.php
@@ -37,9 +37,9 @@
         </div>
     </div>
     <div>
-        <p>Trois derniers avis</p>
+        <p class="font-bold text-xl">Trois derniers avis</p>
         @foreach($lastReview as $data)
-        <div>
+        <div class="border-solid border-2 rounded-md p-2 mb-4">
             <p>{{$data->user->name}} - {{$data->product->name}}</p>
             <div class="flex ">
                 @for($i = 1; $i < 6; $i++)
@@ -58,4 +58,59 @@
         </div>
         @endforeach
     </div>    
+    <table class="w-full text-sm text-left rtl:text-right text-zinc-50 ">
+        <thead class="text-xs text-zinc-50 uppercase bg-accent ">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Nom
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Email
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Adresse
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Rôle
+                </th>
+                  <th scope="col" class="px-6 py-3">
+                    Nombre de commandes
+                </th>
+                <th scope="col" class="px-6 py-3">
+                   Actions
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($users as $user)
+        
+             <tr class="bg-secondary border-b text-slate-950">
+                <th scope="row" class="px-6 py-4 font-bold text-slate-950 whitespace-nowrap ">
+                   {{$user->name}}
+                </th>
+                <td class="px-6 py-4">
+                    {{$user->email}}
+                </td>
+                <td class="px-6 py-4">
+                   {{$user->number . " " . $user->street . " " . $user->city ." ". $user->zip_code}}
+                </td>
+                <td class="px-6 py-4">
+                    {{$user->role}}
+                </td>
+                <td class="px-6 py-4">
+                    {{$user->orders->count()}}
+                </td>
+                <td class="px-6 py-4">
+                <a href="{{route("users.editRole", $user)}}">
+                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                    <path fill-rule="evenodd" d="M11.32 6.176H5c-1.105 0-2 .949-2 2.118v10.588C3 20.052 3.895 21 5 21h11c1.105 0 2-.948 2-2.118v-7.75l-3.914 4.144A2.46 2.46 0 0 1 12.81 16l-2.681.568c-1.75.37-3.292-1.263-2.942-3.115l.536-2.839c.097-.512.335-.983.684-1.352l2.914-3.086Z" clip-rule="evenodd"/>
+                    <path fill-rule="evenodd" d="M19.846 4.318a2.148 2.148 0 0 0-.437-.692 2.014 2.014 0 0 0-.654-.463 1.92 1.92 0 0 0-1.544 0 2.014 2.014 0 0 0-.654.463l-.546.578 2.852 3.02.546-.579a2.14 2.14 0 0 0 .437-.692 2.244 2.244 0 0 0 0-1.635ZM17.45 8.721 14.597 5.7 9.82 10.76a.54.54 0 0 0-.137.27l-.536 2.84c-.07.37.239.696.588.622l2.682-.567a.492.492 0 0 0 .255-.145l4.778-5.06Z" clip-rule="evenodd"/>
+                    </svg>
+                </a>
+                </td>
+            </tr>
+            @endforeach
+           
+        </tbody>
+    </table>
 </x-layout>
