2be2c33d970261da7d2e91465834bc672fa42c56
Benjamin <benjamin.langlois@devinfluence.fr>
2024-04-16 21:24:07 +0200

    Hotfix : ajout welcome pour front

--- resources/js/shop/main.tsx
+++ resources/js/shop/main.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 // import './index.css'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Coucou from "./components/Coucou.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
