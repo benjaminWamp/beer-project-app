0a03ca54f25f87a0aad7e905afab26ae2418308e
ClementDuportCESI <clement.duport@viacesi.fr>
2024-04-17 08:58:24 +0200

    fix : edit categories

--- resources/views/category/edit.blade.php
+++ resources/views/category/edit.blade.php
@@ -44,7 +44,7 @@
                 <div class="text-red-500">{{$message}}</div>
                 @enderror
                 <div class="mt-2.5">
-                    <textarea name="description" id="description" class="block border w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">{{$category->description}}"</textarea>
+                    <textarea name="description" id="description" class="block border w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">{{$category->description}}</textarea>
                 </div>
             </div>
             <div class="mb-4">
--- routes/web.php
+++ routes/web.php
@@ -20,37 +20,13 @@ use App\Models\Review;
 |
 */
 
-Route::get(
-    '/admin/categories',
-    CategoryController::class . "@index"
-)->name("category.index");
-
-Route::get(
-    '/admin/categories/ajouter',
-    CategoryController::class . "@create"
-)->name("category.create");
-
-Route::get(
-    '/admin/categories/{category}',
-    CategoryController::class . "@show"
-)->name("category.show");
-
-Route::delete(
-    '/admin/categories/{category}',
-    CategoryController::class . "@destroy"
-)->name("category.destroy");
-
-Route::post(
-    '/admin/categories',
-    CategoryController::class . "@store"
-)->name("category.store");
-
-Route::get(
-    '/admin/categories/modifier/{category}',
-    CategoryController::class . "@edit"
-)->name("category.edit");
-
-Route::put('/admin/modifier/{category}', CategoryController::class . "@update")->name("category.update");
+Route::get('/admin/categories', CategoryController::class . "@index")->name("category.index");
+Route::get('/admin/categories/ajouter', CategoryController::class . "@create")->name("category.create");
+Route::get('/admin/categories/{category}', CategoryController::class . "@show")->name("category.show");
+Route::delete('/admin/categories/{category}', CategoryController::class . "@destroy")->name("category.destroy");
+Route::post('/admin/categories', CategoryController::class . "@store")->name("category.store");
+Route::get('/admin/categories/modifier/{category}', CategoryController::class . "@edit")->name("category.edit");
+Route::put('/admin/categories/modifier/{category}', CategoryController::class . "@update")->name("category.update");
 
 Route::get('/admin/catalogue', "App\Http\Controllers\ProductController@index")->name('product.index');
 Route::get('/admin/produit/modifier/{product}', "App\Http\Controllers\ProductController@edit")->name('product.edit');
