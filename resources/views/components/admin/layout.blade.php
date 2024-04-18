0b6066e9490e624c0e7f09c04bb14733f1216889
ClementDuportCESI <clement.duport@viacesi.fr>
2024-04-18 12:06:03 +0200

    feat : redesign layout

--- resources/views/components/admin/layout.blade.php
+++ resources/views/components/admin/layout.blade.php
@@ -13,12 +13,15 @@
 
           <nav class="fixed top-0 z-50 w-full bg-secondary border-b border-gray-200 ">
             <div class="px-3 py-3 lg:px-5 lg:pl-3">
-              <div class="flex items-center justify-between">
+              <div class="flex justify-between items-center">
                 <div class="flex items-center justify-start rtl:justify-end">
                   <a href="#" class="flex gap-4 ms-2 md:me-24">
                     <img src="{{asset("storage\app\public\logos\MB_Logo.png")}}" alt="Logo Monsieur Bière" class="w-10">
                     <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap font-title">Monsieur Bière</span>
                   </a>
+                </div>
+                <div>
+                  <p class="font-title font-bold text-l">Bonjour {{Auth::user()->name}}</p>
                 </div>
               </div>
             </div>
