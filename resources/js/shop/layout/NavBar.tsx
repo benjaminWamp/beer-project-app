829ead81062b9121849abc1daddf0eb6fc3350cd
ClementDuportCESI <clement.duport@viacesi.fr>
2024-04-19 13:23:48 +0200

    feat : contact page

--- resources/js/shop/layout/NavBar.tsx
+++ resources/js/shop/layout/NavBar.tsx
@@ -7,7 +7,7 @@ const NavBar = () => {
     const { isLogged, setIsLogged } = useContext(UserContext);
 
     return (
-        <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <nav className="bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a
                     href="https://flowbite.com/"
