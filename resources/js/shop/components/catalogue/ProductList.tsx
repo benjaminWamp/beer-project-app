7ad0cf7b7b38326675a757d936b382f70eeec64e
CecileVexe <cecile.valte@gmail.com>
2024-04-19 15:32:41 +0200

    feat(react): add catalogue image

--- resources/js/shop/components/catalogue/ProductList.tsx
+++ resources/js/shop/components/catalogue/ProductList.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
 import ProductCategories from "./ProdcutCategories";
 import ReviewsStars from "../shared/ReviewsStars";
 import { Product } from "../../types/product.types";
+import UserContext from "../../context/Context";
 
 interface productListProps {
     products: Array<Product>;
@@ -9,6 +10,7 @@ interface productListProps {
 
 const ProductList = (props: productListProps) => {
     const { products } = props;
+    const { url } = useContext(UserContext);
 
     return (
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
@@ -21,7 +23,7 @@ const ProductList = (props: productListProps) => {
                     >
                         <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                             <img
-                                src={product.image}
+                                src={`${url}/storage/images/${product.image}`}
                                 alt={product.name}
                                 className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                             />
