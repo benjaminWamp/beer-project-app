3ae0a5abda1bb8c073831bb577c557cff0423e16
CecileVexe <cecile.valte@gmail.com>
2024-05-05 17:57:30 +0200

    feat(react): resolve getFavorite params issue

--- resources/js/shop/utils/services/FavoriteService.ts
+++ resources/js/shop/utils/services/FavoriteService.ts
@@ -12,7 +12,7 @@ export const fetchUserFavorite = async (
         const response = await fetch(
             `http://127.0.0.1:8000/api/user/favorites${
                 pagination
-                    ? "?" + "pagination=true" + searchParams.toString()
+                    ? "?" + "pagination=true&" + searchParams.toString()
                     : ""
             }`,
             {
