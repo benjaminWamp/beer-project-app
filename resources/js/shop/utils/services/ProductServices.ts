266261ae3c6165bd435711ed64833c364f5a575c
CecileVexe <cecile.valte@gmail.com>
2024-05-08 11:24:33 +0200

    feat(react): add product service to axios

--- resources/js/shop/utils/services/ProductServices.ts
+++ resources/js/shop/utils/services/ProductServices.ts
@@ -1,11 +1,10 @@
+import axios from "axios";
+
 export const fetchProduct = async (id: string) => {
-    try {
-        const response = await fetch(`http://127.0.0.1:8000/api/product/${id}`);
-
-        const jsonData = await response.json();
-
-        return jsonData;
-    } catch (error) {
-        console.error("Erreur lors de la récupération des données:", error);
-    }
+    return axios
+        .get(`http://127.0.0.1:8000/api/product/${id}`)
+        .then((response) => response.data)
+        .catch((error) =>
+            console.error("Erreur lors de la récupération des données:", error)
+        );
 };
