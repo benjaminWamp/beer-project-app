a77ad4bd8bd2bd592115478ddf70b9cae997eeb3
CecileVexe <cecile.valte@gmail.com>
2024-05-13 15:00:49 +0200

    fix: local font

--- resources/js/shop/components/shared/H2Bars.tsx
+++ resources/js/shop/components/shared/H2Bars.tsx
@@ -11,14 +11,19 @@ const H2Bars = (props: H2BarsProps) => {
     const { textColor } = props;
     const { text } = props;
 
-
-    return <>
-        <div className="flex items-center">
-            <hr className={`grow border-b-1 ${hrColor}`} />
-            <h2 className={`px-6 text-6xl font-bold font-title ${textColor}`}>{text}</h2>
-            <hr className={`grow border-b-1 ${hrColor}`} />
-        </div>
-    </>
-}
+    return (
+        <>
+            <div className="flex items-center">
+                <hr className={`grow border-b-1 ${hrColor}`} />
+                <h2
+                    className={`px-6 text-6xl font-bold font-title ${textColor}`}
+                >
+                    {text}
+                </h2>
+                <hr className={`grow border-b-1 ${hrColor}`} />
+            </div>
+        </>
+    );
+};
 
 export default H2Bars;
