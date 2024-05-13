758082a132be5e0101057dc4909a60fc2d774bfd
ClementDuportCESI <clement.duport@viacesi.fr>
2024-05-13 13:24:48 +0200

    feat : favicon backend

--- resources/views/components/admin/layout.blade.php
+++ resources/views/components/admin/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? 'Monsieur Bière' }}</title>
-    <link rel="icon" type="image/x-icon" href="images/mb-logo.png">
+    <link rel="icon" type="image/x-icon" href="/images/mb-logo.png">
     @vite( ['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-offWhite">
