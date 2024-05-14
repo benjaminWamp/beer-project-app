f45b4369ec99c09afeddbc27da677dc8deb8b269
Benjamin <benjamin.langlois@devinfluence.fr>
2024-05-14 16:05:29 +0200

    Feat : bouton modifier sur le fichier show

--- resources/views/Product/show.blade.php
+++ resources/views/Product/show.blade.php
@@ -14,6 +14,13 @@
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14M5 12l4-4m-4 4 4 4"/>
             </svg>
         </a>
+        <div class="flex">
+            <a href="{{route("product.edit", $product)}}" class="hover:-translate-y-1 transition-all font-title border bg-accent text-secondary rounded-3xl px-3 py-3 text-sm font-medium">
+                <svg class="w-6 h-6 text-background" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"/>
+                </svg>
+            </a>
+        </div>
         <form method="post" action="{{route('product.destroy', $product)}}">
             @method("DELETE")
             @csrf 
@@ -34,85 +41,36 @@
             @if($product->image)
                 <img src="{{asset("storage/images/$product->image")}}" alt="Image {{$product->name}}" class="mt-2"/>
             @endif
-            <div class="flex">
-                <a href="{{route("product.edit", $product)}}" class="hover:-translate-y-1 transition-all font-title border bg-accent text-secondary rounded-3xl px-3 py-3 text-sm font-medium">
-                    <svg class="w-6 h-6 text-background" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"/>
-                    </svg>
-                </a>
-            </div>
         </div>
 
         <h2 class="mt-4 font-title text-2xl leading-none font-semibold leading-6 text-accent">Prix unitaire&nbsp;:</h2> 
         <div class="flex align-center mt-2 gap-4">
             <p class="max-w-screen-lg bg-table border-b border-accent p-2 rounded-md">{{$product->price_ht / 100}} €</p>
-            <div class="flex">
-                <a href="{{route("product.edit", $product)}}" class="max-h-12 hover:-translate-y-1 transition-all font-title border bg-accent text-secondary rounded-3xl px-3 py-3 text-sm font-medium">
-                    <svg class="w-6 h-6 text-background" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"/>
-                    </svg>
-                </a>
-            </div>
         </div>
 
         <h2 class="mt-4 font-title text-2xl leading-none font-semibold leading-6 text-accent">Description&nbsp;:</h2> 
         <div class="flex align-center mt-2 gap-4">
             <p class="max-w-screen-lg bg-table border-b border-accent p-2 rounded-md">{{$product->description}}</p>
-            <div class="flex">
-                <a href="{{route("product.edit", $product)}}" class="max-h-12 hover:-translate-y-1 transition-all font-title border bg-accent text-secondary rounded-3xl px-3 py-3 text-sm font-medium">
-                    <svg class="w-6 h-6 text-background" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"/>
-                    </svg>
-                </a>
-            </div>
         </div>
 
         <h2 class="mt-4 font-title text-2xl leading-none font-semibold leading-6 text-accent">Créé le&nbsp;:</h2> 
         <div class="flex align-center mt-2 gap-4">
             <p class="max-w-screen-lg bg-table border-b border-accent p-2 rounded-md">{{$product->created_at->format("d/m/Y")}}</p>
-            <div class="flex">
-                <a href="{{route("product.edit", $product)}}" class="max-h-12 hover:-translate-y-1 transition-all font-title border bg-accent text-secondary rounded-3xl px-3 py-3 text-sm font-medium">
-                    <svg class="w-6 h-6 text-background" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"/>
-                    </svg>
-                </a>
-            </div>
         </div>
 
         <h2 class="mt-4 font-title text-2xl leading-none font-semibold leading-6 text-accent">En stock&nbsp;:</h2> 
         <div class="flex align-center mt-2 gap-4">
             <p class="max-w-screen-lg bg-table border-b border-accent p-2 rounded-md">{{$product->stock}}</p>
-            <div class="flex">
-                <a href="{{route("product.edit", $product)}}" class="max-h-12 hover:-translate-y-1 transition-all font-title border bg-accent text-secondary rounded-3xl px-3 py-3 text-sm font-medium">
-                    <svg class="w-6 h-6 text-background" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"/>
-                    </svg>
-                </a>
-            </div>
         </div>
 
         <h2 class="mt-4 font-title text-2xl leading-none font-semibold leading-6 text-accent">Avis&nbsp;:</h2> 
         <div class="flex align-center mt-2 gap-4">
             <p class="max-w-screen-lg bg-table border-b border-accent p-2 rounded-md">{{$product->reviews_sum}} / 5</p>
-            <div class="flex">
-                <a href="{{route("product.edit", $product)}}" class="max-h-12 hover:-translate-y-1 transition-all font-title border bg-accent text-secondary rounded-3xl px-3 py-3 text-sm font-medium">
-                    <svg class="w-6 h-6 text-background" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"/>
-                    </svg>
-                </a>
-            </div>
         </div>
 
         <h2 class="mt-4 font-title text-2xl leading-none font-semibold leading-6 text-accent">Producteur&nbsp;:</h2> 
         <div class="flex align-center mt-2 gap-4">
             <p class="max-w-screen-lg bg-table border-b border-accent p-2 rounded-md">{{$product->manufacturer->name}}</p>
-            <div class="flex">
-                <a href="{{route("product.edit", $product)}}" class="max-h-12 hover:-translate-y-1 transition-all font-title border bg-accent text-secondary rounded-3xl px-3 py-3 text-sm font-medium">
-                    <svg class="w-6 h-6 text-background" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"/>
-                    </svg>
-                </a>
-            </div>
         </div>
         
     </div>
--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -13,6 +13,13 @@
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14M5 12l4-4m-4 4 4 4"/>
             </svg>
         </a>
+        <div class="flex">
+            <a href="{{route("category.edit", $category)}}" class="max-h-12 hover:-translate-y-1 transition-all font-title border bg-accent text-secondary rounded-3xl px-3 py-3 text-sm font-medium">
+                <svg class="w-6 h-6 text-background" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"/>
+                </svg>
+            </a>
+        </div>
         <form method="post" action="{{route('category.destroy', $category)}}">
             @method("DELETE")
             @csrf 
@@ -30,37 +37,16 @@
         <h2 class="mt-4 font-title text-2xl leading-none font-semibold leading-6 text-accent">Slug&nbsp;:</h2>
         <div class="flex align-center mt-2 gap-4">
             <p class="max-w-screen-lg bg-table border-b border-accent p-2 rounded-md">{{$category->slug}}</p>
-            <div class="flex">
-                <a href="{{route("category.edit", $category)}}" class="max-h-12 hover:-translate-y-1 transition-all font-title border bg-accent text-secondary rounded-3xl px-3 py-3 text-sm font-medium">
-                    <svg class="w-6 h-6 text-background" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"/>
-                    </svg>
-                </a>
-            </div>
         </div>
 
         <h2 class="mt-4 font-title text-2xl leading-none font-semibold leading-6 text-accent">Description&nbsp;:</h2>
         <div class="flex align-center mt-2 gap-4">
             <p class="max-w-screen-lg bg-table border-b border-accent p-2 rounded-md">{{$category->description}}</p>
-            <div class="flex">
-                <a href="{{route("category.edit", $category)}}" class="max-h-12 hover:-translate-y-1 transition-all font-title border bg-accent text-secondary rounded-3xl px-3 py-3 text-sm font-medium">
-                    <svg class="w-6 h-6 text-background" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"/>
-                    </svg>
-                </a>
-            </div>
         </div>
 
         <h2 class="mt-4 font-title text-2xl leading-none font-semibold leading-6 text-accent">Couleur de la catégorie&nbsp;:</h2>
         <div class="flex align-center mt-2 gap-4">
             <div style="background-color: {{$category->color}}" class="w-12 h-12 border-current rounded"></div>
-            <div class="flex">
-                <a href="{{route("category.edit", $category)}}" class="max-h-12 hover:-translate-y-1 transition-all font-title border bg-accent text-secondary rounded-3xl px-3 py-3 text-sm font-medium">
-                    <svg class="w-6 h-6 text-background" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"/>
-                    </svg>
-                </a>
-            </div>
         </div>
 
     </div>
